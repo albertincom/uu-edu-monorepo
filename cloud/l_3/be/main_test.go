14175741fe1fcd3fe5c782187fde15bf7c02a6f4
C5323037 <oleksandr.bunin@sap.com>
2024-11-24 14:57:48 +0200

    init/cloud-l_3/Add GitHub Actions pipeline for Go tests

--- cloud/l_3/be/main_test.go
+++ cloud/l_3/be/main_test.go
@@ -1,1 +1,23 @@
 package main
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	testCases := []struct {
+		a, b     int
+		expected int
+	}{
+		{a: 1, b: 2, expected: 3},
+		{a: -1, b: 5, expected: 4},
+		{a: 0, b: 0, expected: 0},
+		{a: -3, b: -7, expected: -10},
+		{a: 100, b: 200, expected: 300},
+	}
+
+	for _, tc := range testCases {
+		result := Add(tc.a, tc.b)
+		if result != tc.expected {
+			t.Errorf("Add(%d, %d) = %d; want %d", tc.a, tc.b, result, tc.expected)
+		}
+	}
+}
