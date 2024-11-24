d004b037d1bd2f96432f3d27ddd4eb06f62603be
C5323037 <oleksandr.bunin@sap.com>
2024-11-24 14:14:47 +0200

    fix/cloud-l_2

--- cloud/l_2/be/buildAndPushDockerImage.sh
+++ cloud/l_2/be/buildAndPushDockerImage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TAG_VERSION="0.1.0-rc2"
+TAG_VERSION="0.1.0-rc4"
 
 docker build -t "sasha192bunin/uu_edu_monorepo_cloud_l2:$TAG_VERSION" -t sasha192bunin/uu_edu_monorepo_cloud_l2:latest . && \
 docker push "sasha192bunin/uu_edu_monorepo_cloud_l2:$TAG_VERSION" && \
