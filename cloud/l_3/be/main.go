5d9104050f74e1ce6500640cc6ca571574e26b6d
C5323037 <oleksandr.bunin@sap.com>
2024-11-24 15:13:17 +0200

    mod/cloud-l_3/minor-changes

--- cloud/l_3/be/main.go
+++ cloud/l_3/be/main.go
@@ -1,5 +1,34 @@
 package main
 
+import "fmt"
+
 func Add(a int, b int) int {
+
 	return a + b
+}
+
+func Multiply(a int, b int) int {
+
+	return a * b
+}
+
+func Divide(a int, b int) (int, error) {
+
+	if b == 0 {
+		return 0, fmt.Errorf("division by zero")
+	}
+	return a / b, nil
+}
+
+func main() {
+
+	fmt.Println("Addition:", Add(10, 5))            // 15
+	fmt.Println("Multiplication:", Multiply(10, 5)) // 50
+
+	result, err := Divide(10, 2)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Division:", result) // 5
+	}
 }
--- cloud/l_3/be/main_test.go
+++ cloud/l_3/be/main_test.go
@@ -4,20 +4,57 @@ import "testing"
 
 func TestAdd(t *testing.T) {
 	testCases := []struct {
-		a, b     int
-		expected int
+		a, b, expected int
 	}{
-		{a: 1, b: 2, expected: 3},
-		{a: -1, b: 5, expected: 4},
-		{a: 0, b: 0, expected: 0},
-		{a: -3, b: -7, expected: -10},
-		{a: 100, b: 200, expected: 300},
+		{1, 2, 3},
+		{5, -3, 2},
+		{0, 0, 0},
+		{-1, -1, -2},
 	}
 
 	for _, tc := range testCases {
 		result := Add(tc.a, tc.b)
 		if result != tc.expected {
 			t.Errorf("Add(%d, %d) = %d; want %d", tc.a, tc.b, result, tc.expected)
+		}
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	testCases := []struct {
+		a, b, expected int
+	}{
+		{2, 3, 6},
+		{0, 5, 0},
+		{-1, 4, -4},
+		{-2, -3, 6},
+	}
+
+	for _, tc := range testCases {
+		result := Multiply(tc.a, tc.b)
+		if result != tc.expected {
+			t.Errorf("Multiply(%d, %d) = %d; want %d", tc.a, tc.b, result, tc.expected)
+		}
+	}
+}
+
+func TestDivide(t *testing.T) {
+	testCases := []struct {
+		a, b       int
+		expected   int
+		shouldFail bool
+	}{
+		{6, 2, 3, false},
+		{5, 0, 0, true}, // Division by zero case
+		{-10, -2, 5, false},
+	}
+
+	for _, tc := range testCases {
+		result, err := Divide(tc.a, tc.b)
+		if tc.shouldFail && err == nil {
+			t.Errorf("Divide(%d, %d) expected an error but got none", tc.a, tc.b)
+		} else if !tc.shouldFail && result != tc.expected {
+			t.Errorf("Divide(%d, %d) = %d; want %d", tc.a, tc.b, result, tc.expected)
 		}
 	}
 }
